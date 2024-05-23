8ae20e8fdaf5c279a1b32156daa465864a89990a
ayan <mohdayanrehman07@gmail.com>
2024-05-23 22:50:30 +0530

    remove-cart-item-functionality-ui

--- lib/hooks/useCartStore.tsx
+++ lib/hooks/useCartStore.tsx
@@ -54,7 +54,24 @@ export const cartStore = create<Cart>()(
                     taxPrice,
                     totalPrice,
                   })
-            }
+            },
+            decrease: (item: OrderItem) => {
+              const exist = items.find((x) => x.slug === item.slug)
+              if (!exist) return
+              const updatedCartItems =
+                exist.qty === 1
+                  ? items.filter((x: OrderItem) => x.slug !== item.slug)
+                  : items.map((x) => (item.slug ? { ...exist, qty: exist.qty - 1 } : x))
+              const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
+                calcPrice(updatedCartItems)
+              cartStore.setState({
+                items: updatedCartItems,
+                itemsPrice,
+                shippingPrice,
+                taxPrice,
+                totalPrice,
+              })
+            },
           }
     }
     const calcPrice = (items: OrderItem[]) => {
